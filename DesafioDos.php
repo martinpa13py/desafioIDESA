f26fa1d022715c9e1e2981033828288eb1b7b67a
gabdevpy <104218802+gabdevpy@users.noreply.github.com>
2022-11-16 17:04:41 -0300

    comentarios solucion DesafioDos

--- DesafioDos.php
+++ DesafioDos.php
@@ -3,14 +3,14 @@ require_once 'Database.php';
 
 class DesafioDos {
 
-    public static function retriveLotes(string $loteID):void {
+    public static function retriveLotes(string $loteID):void { //bug corregido, se cambio tipo de parametro a string
 
         Database::setDB(); 
 
         echo(json_encode(self::getLotes($loteID)));
     }
 
-    private static function getLotes (string $loteID){
+    private static function getLotes (string $loteID){ //bug corregido, se cambio tipo de parametro a string
         $lotes = [];
         $cnx = Database::getConnection();
         $stmt = $cnx->query("SELECT * FROM debts WHERE lote = '$loteID' LIMIT 2");
