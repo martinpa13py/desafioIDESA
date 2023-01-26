1c41de3f4df169520d1d46f362796c90c7c82169
edujav <edujav21@gmail.com>
2023-01-26 09:25:54 -0300

    added simple rest api

--- DesafioTres.php
+++ DesafioTres.php
@@ -3,78 +3,59 @@ require_once 'Database.php';
 
 class DesafioTres {
 
-    public static function getLoteInfo(string $loteID): void {
+    public static function getLoteInfo(string $loteID): array {
         // estructura de respuesta para mantener formato de otros desafios
-        $result = array(
-            "status" => true,
-            "message" => "",
+        $data = array(
+            "overDueDebts" => array(
+                "lotes" => array(),
+                "totalAmount" => 0
+            ),
+            "unmaturedDebts" => array(
+                "lotes" => array(),
+                "totalAmount" => 0
+            ),
+            "debtsWithoutExpiricy" => array(
+                "lotes" => array(),
+                "totalAmount" => 0
+            ),
+            "nextDue" => array(
+                "lote" => null,
+                "remainingDays" => null
+            )
         );
-        try {
-            // estructura interna de resultados
-            $data = array(
-                "overDueDebts" => array(
-                    "lotes" => array(),
-                    "totalAmount" => 0
-                ),
-                "unmaturedDebts" => array(
-                    "lotes" => array(),
-                    "totalAmount" => 0
-                ),
-                "debtsWithoutExpiricy" => array(
-                    "lotes" => array(),
-                    "totalAmount" => 0
-                ),
-                "nextDue" => array(
-                    "lote" => null,
-                    "remainingDays" => null
-                )
-            );
-            Database::setDB();
-            $lotes = self::getAllDebtsForLote($loteID);
-            $remainingDays = null;
-            $today = new DateTime(); //fecha al momento de las comparaciones
-            if (count($lotes) > 0) {
-                foreach ($lotes as $loteInfo) {
-                    // si no tiene fecha de vencimiento agrupar y continuar
-                    if (!$loteInfo->vencimiento) {
-                        $data['debtsWithoutExpiricy']['totalAmount'] += $loteInfo->precio;
-                        array_push($data['debtsWithoutExpiricy']['lotes'], $loteInfo);
-                        continue;
-                    }
-                    $vencimiento = new DateTime($loteInfo->vencimiento); // convertir fecha:string a fecha::Datetime para comapracion
-                    // si la fecha de vencimiento ya caducó entonces agrupar
-                    if ($vencimiento < $today) {
-                        $data['overDueDebts']['totalAmount'] += $loteInfo->precio;
-                        array_push($data['overDueDebts']['lotes'], $loteInfo);
-                        // si la fecha de vencimiento aun no pasó entonces agrupar
-                    } else if ($vencimiento > $today) {
-                        $data['unmaturedDebts']['totalAmount'] += $loteInfo->precio;
-                        array_push($data['unmaturedDebts']['lotes'], $loteInfo);
-                        $calculatedRemainingDays = (int) $today->diff($vencimiento)->format("%a"); // obtener intervalo entre 2 fechas en dias
-                        // el proximo vencimiento es la que tiene menor intervalo de dias con respecto a la variable $today
-                        if ($remainingDays === null || $calculatedRemainingDays < $remainingDays) {
-                            $remainingDays = $calculatedRemainingDays;
-                            $data['nextDue']['lote'] = $loteInfo;
-                            $data['nextDue']['remainingDays'] = $remainingDays;
-                        }
+
+        Database::setDB();
+        $lotes = self::getAllDebtsForLote($loteID);
+        $remainingDays = null;
+        $today = new DateTime(); //fecha al momento de las comparaciones
+        if (count($lotes) > 0) {
+            foreach ($lotes as $loteInfo) {
+                // si no tiene fecha de vencimiento agrupar y continuar
+                if (!$loteInfo->vencimiento) {
+                    $data['debtsWithoutExpiricy']['totalAmount'] += $loteInfo->precio;
+                    array_push($data['debtsWithoutExpiricy']['lotes'], $loteInfo);
+                    continue;
+                }
+                $vencimiento = new DateTime($loteInfo->vencimiento); // convertir fecha:string a fecha::Datetime para comapracion
+                // si la fecha de vencimiento ya caducó entonces agrupar
+                if ($vencimiento < $today) {
+                    $data['overDueDebts']['totalAmount'] += $loteInfo->precio;
+                    array_push($data['overDueDebts']['lotes'], $loteInfo);
+                    // si la fecha de vencimiento aun no pasó entonces agrupar
+                } else if ($vencimiento > $today) {
+                    $data['unmaturedDebts']['totalAmount'] += $loteInfo->precio;
+                    array_push($data['unmaturedDebts']['lotes'], $loteInfo);
+                    $calculatedRemainingDays = (int) $today->diff($vencimiento)->format("%a"); // obtener intervalo entre 2 fechas en dias
+                    // el proximo vencimiento es la que tiene menor intervalo de dias con respecto a la variable $today
+                    if ($remainingDays === null || $calculatedRemainingDays < $remainingDays) {
+                        $remainingDays = $calculatedRemainingDays;
+                        $data['nextDue']['lote'] = $loteInfo;
+                        $data['nextDue']['remainingDays'] = $remainingDays;
                     }
                 }
             }
-            // obtener de otra tabla cuando exista
-            $result['information'] = array(
-                "loteID" => $loteID,
-                "name" => "name",
-                "city" => "city"
-            );
-            $result['data'] = $data;
-        } catch (\Throwable $th) {
-            $result['status'] = "failed";
-            $result['message'] = $th->getMessage();
-            echo (json_encode($result));
         }
-
-        echo json_encode($result);
-        exit;
+        return $data;
     }
 
     private static function getAllDebtsForLote(string $loteID): array {
@@ -91,5 +72,3 @@ class DesafioTres {
         return $lotes;
     }
 }
-
-DesafioTres::getLoteInfo('00148');
---
+++ rest/DesafioTresConsulta.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../DesafioTres.php';
+
+$loteID = trim(stripslashes(htmlspecialchars($_GET['id_lote']))); // clean param
+$response = array("status" => "success", "message" => "");
+if (!is_numeric($loteID)) {
+    header("HTTP/1.1 400 Bad Request");
+    header('Content-Type: application/json');
+
+    echo json_encode(array("message" => "Debe especificar in ID de lote"));
+    exit;
+}
+
+try {
+    $loteInfo = DesafioTres::getLoteInfo($loteID);
+    $response['data'] = $loteInfo;
+} catch (\Throwable $th) {
+    $response['status'] = "failed";
+    $response['message'] = $th->getMessage();
+}
+
+if ($response['status'] === "failed") {
+    header("HTTP/1.1 500 Server Error");
+    header('Content-Type: application/json');
+
+    echo json_encode(array("message" => $response['message']));
+    exit;
+}
+
+header('Content-Type: application/json');
+echo json_encode(array("data" => $response['data']));
